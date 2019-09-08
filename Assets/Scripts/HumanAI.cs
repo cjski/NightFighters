d9aebc9649b217483f335337bd4f1c7036f1c3fc
Chris <chrisfjski@gmail.com>
2019-09-08 18:20:34 -0400

    Made AI code more modular

--- Assets/Scripts/HumanAI.cs
+++ Assets/Scripts/HumanAI.cs
@@ -7,6 +7,10 @@ public class HumanAI : AI
     static Map map;
     static float lightTargetDistanceOffset = 50; //Offsets the distance for the lights so the AI is more likely to target players
     private Vector2 finalDirection = new Vector2(0, 0);
+    private float distanceToTargetSquared;
+    private Node selfNode;
+    private Vector2 selfPosition;
+    private Vector2 direction;
     private float range = 3;
 
     protected new void Start()
@@ -30,7 +34,7 @@ public class HumanAI : AI
         {
             bool canSeeTarget = false;
             bool isPlayer = false;
-            GetDirectionToTargetForMovement(ref finalDirection, ref canSeeTarget, ref isPlayer);
+            finalDirection = GetDirectionToTargetForMovement(ref canSeeTarget, ref isPlayer);
             if (canSeeTarget && isPlayer && playerController.primaryCooldown.done)
             {
                 playerController.AIUsePrimary();
@@ -40,18 +44,18 @@ public class HumanAI : AI
         }
     }
 
-    void GetDirectionToTargetForMovement( ref Vector2 direction, ref bool canSeeTarget, ref bool isPlayer )
+    Vector2 GetDirectionToTargetForMovement(ref bool canSeeTarget, ref bool isPlayer )
     {
         direction = Vector2.zero;
         
         canSeeTarget = false;
         
-        Node selfNode = map.GetNode(playerController.gameObject.transform.position);
+        selfNode = map.GetNode(playerController.gameObject.transform.position);
 
-        float distanceToTargetSquared = 9999;
+        distanceToTargetSquared = 9999;
         Vector2 targetPosition = Vector2.zero;
         Vector2 toTarget = Vector2.zero;
-        Vector2 selfPosition = playerController.gameObject.transform.position;
+        selfPosition = playerController.gameObject.transform.position;
 
         // Have to get these each time because some players may die and then they leave the array
         GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
@@ -63,52 +67,9 @@ public class HumanAI : AI
             MonsterController mc = players[i].GetComponent<MonsterController>();
             if (mc != null)
             {
-                targetPosition = mc.gameObject.transform.position;
-                toTarget = targetPosition - selfPosition;
-                float targetDistanceSquared = toTarget.sqrMagnitude;
-                if (targetDistanceSquared < distanceToTargetSquared)
+                if(IsNewTargetCloser(mc.gameObject, ref direction, ref canSeeTarget, ignoreLightLanternLayerMask))
                 {
-                    Vector2 newTargetDirection = Vector2.zero;
-                    canSeeTarget = FindIfTargetIsVisible(targetPosition, toTarget, ref newTargetDirection, ignoreLightLanternLayerMask);
-                    if(!canSeeTarget)
-                    {
-                        Node targetNode = map.GetNode(targetPosition);
-                        // Add one to the target distance calculation because if they are on the same node it will count as 0
-                        float targetDistance = (selfNode.distances[targetNode.x, targetNode.y] + 1) * map.unitSize;
-                        targetDistanceSquared = targetDistance * targetDistance;
-                        if (targetDistanceSquared < distanceToTargetSquared)
-                        {
-                            Node destination = selfNode;
-
-                            // Pick the next best node beside you to go to
-                            if (selfNode.l != Node.Connection.Wall && map.GetNode(selfNode.x - 1, selfNode.y).distances[targetNode.x, targetNode.y] < destination.distances[targetNode.x, targetNode.y])
-                            {
-                                destination = map.GetNode(selfNode.x - 1, selfNode.y);
-                            }
-                            if (selfNode.d != Node.Connection.Wall && map.GetNode(selfNode.x, selfNode.y - 1).distances[targetNode.x, targetNode.y] < destination.distances[targetNode.x, targetNode.y])
-                            {
-                                destination = map.GetNode(selfNode.x, selfNode.y - 1);
-                            }
-                            if (selfNode.u != Node.Connection.Wall && map.GetNode(selfNode.x, selfNode.y + 1).distances[targetNode.x, targetNode.y] < destination.distances[targetNode.x, targetNode.y])
-                            {
-                                destination = map.GetNode(selfNode.x, selfNode.y + 1);
-                            }
-                            if (selfNode.r != Node.Connection.Wall && map.GetNode(selfNode.x + 1, selfNode.y).distances[targetNode.x, targetNode.y] < destination.distances[targetNode.x, targetNode.y])
-                            {
-                                destination = map.GetNode(selfNode.x + 1, selfNode.y);
-                            }
-
-                            direction = map.GetRealNodePosition(destination.x, destination.y) - selfPosition;
-                            distanceToTargetSquared = targetDistanceSquared;
-                            isPlayer = true;
-                        }
-                    }
-                    else
-                    {
-                        direction = newTargetDirection;
-                        distanceToTargetSquared = targetDistanceSquared;
-                        isPlayer = true;
-                    }
+                    isPlayer = true;
                 }
             }
         }
@@ -119,53 +80,66 @@ public class HumanAI : AI
             // Don't go for any lights that another human is turning on already, don't go for lanterns
             if (lc != null && !lc.On() && !lc.IsLantern() && (!lc.humansIn || lc.currentHumanInLight == playerController.gameObject))
             {
-                targetPosition = lc.gameObject.transform.position;
-                toTarget = targetPosition - selfPosition;
-                float targetDistanceSquared = toTarget.sqrMagnitude + lightTargetDistanceOffset;
-                if (targetDistanceSquared < distanceToTargetSquared)
+                if(IsNewTargetCloser(lc.gameObject, ref direction, ref canSeeTarget, ignoreLanternLayerMask, lightTargetDistanceOffset))
                 {
-                    Vector2 newTargetDirection = Vector2.zero;
-                    canSeeTarget = FindIfTargetIsVisible(targetPosition, toTarget, ref newTargetDirection, ignoreLanternLayerMask);
-                    if (!canSeeTarget)
-                    {
-                        Node targetNode = map.GetNode(targetPosition);
-                        float targetDistance = (selfNode.distances[targetNode.x, targetNode.y] + 1) * map.unitSize;
-                        targetDistanceSquared = targetDistance * targetDistance + lightTargetDistanceOffset;
-                        if (targetDistanceSquared < distanceToTargetSquared)
-                        {
-                            Node destination = selfNode;
-
-                            // Pick the next best node beside you to go to
-                            if (selfNode.l != Node.Connection.Wall && map.GetNode(selfNode.x - 1, selfNode.y).distances[targetNode.x, targetNode.y] < destination.distances[targetNode.x, targetNode.y])
-                            {
-                                destination = map.GetNode(selfNode.x - 1, selfNode.y);
-                            }
-                            if (selfNode.d != Node.Connection.Wall && map.GetNode(selfNode.x, selfNode.y - 1).distances[targetNode.x, targetNode.y] < destination.distances[targetNode.x, targetNode.y])
-                            {
-                                destination = map.GetNode(selfNode.x, selfNode.y - 1);
-                            }
-                            if (selfNode.u != Node.Connection.Wall && map.GetNode(selfNode.x, selfNode.y + 1).distances[targetNode.x, targetNode.y] < destination.distances[targetNode.x, targetNode.y])
-                            {
-                                destination = map.GetNode(selfNode.x, selfNode.y + 1);
-                            }
-                            if (selfNode.r != Node.Connection.Wall && map.GetNode(selfNode.x + 1, selfNode.y).distances[targetNode.x, targetNode.y] < destination.distances[targetNode.x, targetNode.y])
-                            {
-                                destination = map.GetNode(selfNode.x + 1, selfNode.y);
-                            }
-
-                            direction = map.GetRealNodePosition(destination.x, destination.y) - selfPosition;
-                            distanceToTargetSquared = targetDistanceSquared;
-                            isPlayer = false;
-                        }
-                    }
-                    else
-                    {
-                        direction = newTargetDirection;
-                        distanceToTargetSquared = targetDistanceSquared;
-                        isPlayer = false;
-                    }
+                    isPlayer = false;
                 }
             }
         }
+
+        return direction;
+    }
+
+    bool IsNewTargetCloser(GameObject targetObject, ref Vector2 direction, ref bool canSeeTarget, int layerMask, float distanceOffset = 0)
+    {
+        Vector3 targetPosition = targetObject.transform.position;
+        Vector2 toTarget = targetPosition - playerController.gameObject.transform.position;
+        float targetDistanceSquared = toTarget.sqrMagnitude + distanceOffset;
+        if (targetDistanceSquared < distanceToTargetSquared)
+        {
+            Vector2 newTargetDirection = Vector2.zero;
+            canSeeTarget = FindIfTargetIsVisible(targetPosition, toTarget, ref newTargetDirection, layerMask);
+            if (!canSeeTarget)
+            {
+                Node targetNode = map.GetNode(targetPosition);
+                // Add one to the target distance calculation because if they are on the same node it will count as 0
+                float targetDistance = (selfNode.distances[targetNode.x, targetNode.y] + 1) * map.unitSize;
+                targetDistanceSquared = targetDistance * targetDistance + distanceOffset;
+                if (targetDistanceSquared < distanceToTargetSquared)
+                {
+                    Node destination = selfNode;
+
+                    // Pick the next best node beside you to go to
+                    if (selfNode.l != Node.Connection.Wall && map.GetNode(selfNode.x - 1, selfNode.y).distances[targetNode.x, targetNode.y] < destination.distances[targetNode.x, targetNode.y])
+                    {
+                        destination = map.GetNode(selfNode.x - 1, selfNode.y);
+                    }
+                    if (selfNode.d != Node.Connection.Wall && map.GetNode(selfNode.x, selfNode.y - 1).distances[targetNode.x, targetNode.y] < destination.distances[targetNode.x, targetNode.y])
+                    {
+                        destination = map.GetNode(selfNode.x, selfNode.y - 1);
+                    }
+                    if (selfNode.u != Node.Connection.Wall && map.GetNode(selfNode.x, selfNode.y + 1).distances[targetNode.x, targetNode.y] < destination.distances[targetNode.x, targetNode.y])
+                    {
+                        destination = map.GetNode(selfNode.x, selfNode.y + 1);
+                    }
+                    if (selfNode.r != Node.Connection.Wall && map.GetNode(selfNode.x + 1, selfNode.y).distances[targetNode.x, targetNode.y] < destination.distances[targetNode.x, targetNode.y])
+                    {
+                        destination = map.GetNode(selfNode.x + 1, selfNode.y);
+                    }
+
+                    direction = map.GetRealNodePosition(destination.x, destination.y) - selfPosition;
+                    distanceToTargetSquared = targetDistanceSquared;
+                    return true;
+                }
+            }
+            else
+            {
+                direction = newTargetDirection;
+                distanceToTargetSquared = targetDistanceSquared;
+                return true;
+            }
+        }
+
+        return false;
     }
 }

6c4434eca9020947c8aab3c6441b6d41303aedf7
Chris <chrisfjski@gmail.com>
2019-10-25 18:50:54 -0400

    Added framework for controlling bosses as separate monsters

---
+++ Assets/Scripts/Player/BossController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonsterController
+{
+    protected override void OnPrimaryPressed()
+    {
+
+    }
+
+    protected override void OnSecondaryPressed()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    new protected void Start()
+    {
+        base.Start();
+    }
+
+    // Update is called once per frame
+    new protected void Update()
+    {
+        base.Update();
+    }
+}
---
+++ Assets/Scripts/Player/WerewolfBossController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WerewolfBossController : BossController
+{
+    // Start is called before the first frame update
+    new protected void Start()
+    {
+        baseSpeed = 0.08f;
+        maxHealth = 200;
+        base.Start();
+    }
+
+    // Update is called once per frame
+    new protected void Update()
+    {
+        base.Update();
+    }
+
+    protected override void OnPrimaryPressed()
+    {
+
+    }
+
+    protected override void OnSecondaryPressed()
+    {
+
+    }
+}
